deda3fa794c5ec2016405b330f6544e46f75ab06
Ana Mafla <anamafla@hotmail.com>
2018-08-15 00:24:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  const lis = document
+  .getElementById("app")
+  .querySelectorAll("ul.ranked-list li");
+ 
+for (let i = 0; i < lis.length; i++) {
+  let value = lis[i].innerHTML; 
+  lis[i].innerHTML = parseInt(value) + n;
+  }
+}
+
+function deepestChild(){
+  
+  let node = document.querySelector('#grand-node');
+  let current = node.getElementsByTagName('div');
+  let next=[];
+  
+  
+  while (current.firstElementChild !== null) {
+
+  for (let i = 0; i < current.length; i++){
+    next.push(current[i])
+    }
+  
+   current = next.shift();
+  }
+ 
+ return current; 
+}
